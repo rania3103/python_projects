d16259c652d397ba06d7d38ea4ffa0c7d611a354
rania3103 <raniaabassi33@gmail.com>
2023-11-20 15:53:57 +0100

    open cv ai

--- python_openCV_AI/lesson6.py
+++ python_openCV_AI/lesson6.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """when we press on q we quit the program and we press on s we save the new image"""
 import cv2
-img = cv2.imread("images/baby.jpg")
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert color TO GRAY
-cv2.imshow("rania", gray)
-cv2.waitKey(0)
+
+img = cv2.imread("images/car.jpg", 0)
+cv2.imshow("rania", img)
+k = cv2.waitKey(0)
+
+if k == ord("q"):
+    cv2.destroyAllWindows()
+elif k == ord("s"):
+    cv2.imwrite("images/gray_car.jpg", img)
+    cv2.destroyAllWindows()
