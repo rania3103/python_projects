7a06f534ce23096554951e61e0994f5e9143a973
rania3103 <raniaabassi33@gmail.com>
2023-11-20 16:26:18 +0100

    add rectangle around baby face

--- python_openCV_AI/lesson7.py
+++ python_openCV_AI/lesson7.py
@@ -2,8 +2,9 @@
 """print an image of baby face with a rectangle around his face"""
 import cv2
 img = cv2.imread("images/baby.jpg")
-# top-left, bottom-right, color green, thickness
+# draw green rectangle around a sopecified region (top-left, bottom-right, color green, thickness)
 cv2.rectangle(img, (500, 60), (800, 550), (0, 255, 0), 4)
+#add etxt with blue color
 cv2.putText(img, "baby face", (500, 60),
             cv2.FONT_HERSHEY_TRIPLEX, 2, (255, 0, 0))
 cv2.imshow("rania", img)
