3db925899f3adb7fd929810a1932ab30275dd926
rania3103 <raniaabassi33@gmail.com>
2023-11-22 17:50:01 +0100

    to voice

--- convert_text_to_speech/c1.py
+++ convert_text_to_speech/c1.py
@@ -2,7 +2,7 @@
 """ convert text to speech"""
 from gtts import gTTS
 from playsound import playsound
-txt = "hello my name is rania"
+txt = input("enter your text: ")
 x = gTTS(text= txt)
 x.save("file.mp3") #save txt in file
 playsound("file.mp3") #play the text in file
